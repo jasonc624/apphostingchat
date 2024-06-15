e1eff085018bc1936979c0ad3e1b816bd582ffd4
Jason Cardenas <jasonc62492@gmail.com>
2024-06-15 14:54:19 -0400

    idk

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,22 +3,27 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
 import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+import { provideFunctions, getFunctions, connectFunctionsEmulator } from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
 import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
-
+const firebaseConfig: any = {
+  apiKey: "AIzaSyBfIKzE5tJhADPjcnCX6nPmF72PywpWQRo",
+  authDomain: "firechatssr.firebaseapp.com",
+  projectId: "firechatssr",
+  storageBucket: "firechatssr.appspot.com",
+  messagingSenderId: "956860917205",
+  appId: "1:956860917205:web:b33920e4eac6226eb3895c"
+};
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"firechatssr","appId":"1:956860917205:web:0223fc06dab493dfb3895c","storageBucket":"firechatssr.appspot.com","apiKey":"AIzaSyBfIKzE5tJhADPjcnCX6nPmF72PywpWQRo","authDomain":"firechatssr.firebaseapp.com","messagingSenderId":"956860917205"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging())
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging()),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ "projectId": "firechatssr", "appId": "1:956860917205:web:0223fc06dab493dfb3895c", "storageBucket": "firechatssr.appspot.com", "apiKey": "AIzaSyBfIKzE5tJhADPjcnCX6nPmF72PywpWQRo", "authDomain": "firechatssr.firebaseapp.com", "messagingSenderId": "956860917205" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), provideFirebaseApp(() => initializeApp({"projectId":"firechatssr","appId":"1:956860917205:web:b33920e4eac6226eb3895c","storageBucket":"firechatssr.appspot.com","apiKey":"AIzaSyBfIKzE5tJhADPjcnCX6nPmF72PywpWQRo","authDomain":"firechatssr.firebaseapp.com","messagingSenderId":"956860917205"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
